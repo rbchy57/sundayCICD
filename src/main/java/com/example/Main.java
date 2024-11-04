8ba00a5662b1f506f425aa137f1ea52a8a2371a0
Rbchy57 <chyranajit@gmail.com>
2024-11-03 20:44:10 -0500

    My First Commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //WebDriver driverObject = new  ChromeDriver();//Mozilla// Safari
+        //driverObject.get("https://demo.guru99.com/telecom/addtariffplans.php");
+        //Thread.sleep(1000);
+    }
+}
